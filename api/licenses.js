f79c3c8a121dd3816b77e252c0fb186caaf4b170
ya-xzer21145 <3607483986@qq.com>
2026-07-04 16:37:22 +0800

    Delete api directory

--- api/licenses.js
+++
@@ -1,7 +0,0 @@
-const auth = require('./_lib/auth');
-const db = require('./_lib/db');
-module.exports = auth(async (req, res) => {
-  const database = await db();
-  const licenses = await database.collection('licenses').find({ userId: req.userId }).toArray();
-  res.json(licenses);
-});
